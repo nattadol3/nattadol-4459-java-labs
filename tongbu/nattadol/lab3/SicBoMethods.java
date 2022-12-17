4ed7c154c3ee09540ff7b85c79c51c3a53d6bbac
nattadol <nattadol.t@kkumail.com>
2022-12-17 14:20:18 +0700

    setup

--- tongbu/nattadol/lab3/SicBoMethods.java
+++ tongbu/nattadol/lab3/SicBoMethods.java
@@ -1,41 +1,50 @@
 /**
-* This SicBo program is a gamble game and it's to accept 3 arguments:  The first arguments is the game that user want to play
-* The first game is to guess the total value of 3 dices.  if total value is between 3-10 its count as low
-* if the total value is between 11-18 its count as high.  User will need to input h or H to guess high or l or L
-* to guess low.  If user guess correctly, they'll win 20 Bath.  If they guess incorrectly, they'll lose 10 Bath.
-* 
-* The seccond game is to guess a number and see if that number will match one, two, all dices.  
-* User will need to in put a number betteen 1-6 to guess.  if user guess one of the dices value correctly, they'll win 30 Bath.
-* If user guess two of the dices value correctly, they'll win 60 Bath.  If user guess all of the dice value correctly, they'll
-* win 90 Bath.  If user guess none of the dices value correctly, they'll lose 10 Bath.
-* The bet to play each round of both game is 10 Bath.
+* This SicBoMethods program is the same as SicBo program but now we create 2 methods.  The first method getChoice() is use to 
+* get the choice of the game that user want to play.  When user input which game they want.  The seccond method playGame is called.
+* playGame() is the method contains both the same game as in SicBo program.  The first game required user to guess the total 
+* value of three dices.  3-10 is low and 11-18 is high.  If user guess correctly they'll win 20 Bath.  If not they'll 10 Bath.
+* The seccond game required user to pick a number 1-6, then the dice is rolled.  If one of the dices roll the same number as user
+* have picked.  They'll win 30 Bath.  If two of the dices roll the same number as user have picked.  They'll 60 Bath.  
+* If all of the dices roll the same number as user have picked.  They'll win 90 Bath.  
+* If none of the dices roll the same number as user have picked.  They'll lose 10 Bath.
+* Each round of both game required 10 Bath to play.
 *
 * Author: Nattadol Tongbu
 * ID: 653040445-9
 * Sec: 2
-* Date: December 16, 2022
+* Date: December 17, 2022
 *
 **/
 
 package tongbu.nattadol.lab3;
 
-import java.util.Scanner; // To import Scanner.  It's used to read the arguments that user input.
-public class SicBo {
+import java.util.Scanner; // Import Scanner to read the user input.
+
+public class SicBoMethods {
+    static int choice; // Declare variable choice with static so its can be used across the program.
+    static Scanner reader = new Scanner(System.in); // Create new Scanner with static so its can be use across the program.
     public static void main(String[] args) {
-        int die1 = 1 + (int)(Math.random() * ((6 - 1) + 1));
-        int die2 = 1 + (int)(Math.random() * ((6 - 1) + 1));
-        int die3 = 1 + (int)(Math.random() * ((6 - 1) + 1)); // This 3 three lines is to mimic the random dices roll.
-        int total = die1 + die2 + die3; // Calculate the total value of three random dices roll.
+        getChoice();
+        playGame();
+    }
+    
+    public static void getChoice() {
         System.out.println("Choose how do want to bet:");
         System.out.println("Type 1 for choosing high or low numbers.");
         System.out.println("Type 2 for choosing number between 1-6.");
         System.out.print("Enter yourchoice: ");
-        Scanner reader = new Scanner(System.in);
-        String input = reader.nextLine(); // Read the input to see which game user want to play.
+        choice = reader.nextInt(); // Read the input to see which game user want to play.
+    }
+        // getChoice() method to ask the user for which game they want to play, read the input, and pass the value to variable choice.
 
-        if (input.equals("1")) {
+    public static void playGame() {
+        int die1 = 1 + (int)(Math.random() * ((6 - 1) + 1));
+        int die2 = 1 + (int)(Math.random() * ((6 - 1) + 1));
+        int die3 = 1 + (int)(Math.random() * ((6 - 1) + 1)); // This 3 three lines is to mimic the random dices roll.
+        int total = die1 + die2 + die3; // Calculate the total value of three random dices roll.
+        if (choice == 1) {
             System.out.print("Type in h for high and l for low: ");
-            String highLow = reader.nextLine(); // Read if user guess high or low.
+            String highLow = reader.next(); // Read if user guess high or low.
             if (highLow.equalsIgnoreCase("h")) {
                 System.out.println("Dice 1 : " + die1 + ", Dice 2 : " + die2 + ", Dice 3 : " + die3);
                 System.out.println("Total is " + total);
@@ -62,9 +71,9 @@ public class SicBo {
                 // Error messege in case of if user in put something other than h, H, l, and L.
             }
         }
-        else if (input.equalsIgnoreCase("2")) {
+        else if (choice == 2) { 
             System.out.print("Print in a number to bet on (1-6): ");
-            String betting = reader.nextLine(); // Read which number the user guess.
+            String betting = reader.next(); // Read which number the user guess.
             int bettingNumber = Integer.parseInt(betting);
             if (bettingNumber <= 6 && bettingNumber >= 1) {
                 System.out.println("Dice 1 : " + die1 + ", Dice 2 : " + die2 + ", Dice 3 : " + die3);
@@ -103,7 +112,9 @@ public class SicBo {
         else {
             System.out.println("Incorrect input. Enter 1 or 2 only."); 
             // Error message in case of if user input something other than "1" or "2".
-        }
-        reader.close(); // Close the Scanner.
-    }  
+        } 
+        // playGame() method contains both games.  The game that will be played depend on what the input user type in.
+        // If 1 then the Sic Bo game is played.  If 2 then the guess the dices number is played.
+        reader.close(); // Close the Scanner
+    }
 }
